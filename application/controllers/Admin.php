5343a2ea77aad5f0eb954cc8123dc76c1ac4cb7a
shafaalafghany <shafagabriel1@gmail.com>
2020-02-09 05:54:52 +0700

    Template admin sudah lengkap, namun footer acak acakan 2

---
+++ application/controllers/Admin.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+    public function index()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Dashboard';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/index');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+
+    public function daftar_modul()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Daftar Modul';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/modul/daftar_modul');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+
+    public function tambah_modul()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Tambah Modul';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/modul/tambah_modul');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+
+    public function daftar_event()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Daftar Event';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/event/daftar_event');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+
+    public function daftar_soal()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Daftar Soal';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/event/daftar_soal');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+    public function tambah_event()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Tambah Event';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/event/tambah_event');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+    public function tambah_soal()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Tambah Soal';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/event/tambah_soal');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+    public function daftar_admin()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Daftar Admin';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/admin/daftar_admin');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+    public function tambah_admin()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Tambah Admin';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/admin/tambah_admin');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+    public function daftar_peserta()
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Daftar Peserta';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('Admin/templates/header_admin', $data);
+        $this->load->view('Admin/peserta/daftar_peserta');
+        $this->load->view('Admin/templates/footer_admin');
+    }
+}
