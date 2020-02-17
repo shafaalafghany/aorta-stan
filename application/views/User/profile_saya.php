7ea13eaa228c031cff1c823fef73e0d8791f04c7
brigi123 <brigitatiora1@gmail.com>
2020-02-17 14:45:58 +0700

    Edit header sedikit

--- application/views/User/profile_saya.php
+++ application/views/User/profile_saya.php
@@ -86,7 +86,7 @@
               <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
                 <h4 style="margin-bottom: 20px;"><strong>Tentang Saya</strong></h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  <?= $user['tentang']; ?>
                 </p>
               </div>
               <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
@@ -98,10 +98,8 @@
                   <div style="display: flex;">
                     <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" style="width: 150px; height: 150px; margin-right: 40px;">
                     <div style="display: all; margin-top: 4%">
-                      <form action="/action_page.php">
-                        <label>Foto Profile sebaiknya memiliki rasio 1:1 dan tidak lebih dari 2MB.</label>
-                        <input type="file" id="image" name="image" accept="image/*">
-                      </form>
+                      <label>Foto Profile sebaiknya memiliki rasio 1:1 dan tidak lebih dari 2MB.</label>
+                      <input type="file" id="image" name="image" accept="image/*">
                     </div>
                   </div>
                 </div>
