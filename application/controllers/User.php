6eca300781d9d35231c2973185f8e5f8c8d4b4a4
brigi123 <brigitatiora1@gmail.com>
2020-02-18 22:37:29 +0700

    Membuat halaman tryout tpi belum sempurna

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -39,9 +39,52 @@ class User extends CI_Controller
         $data['judul'] = 'AORTASTAN Try Out Online | Event Detail';
         $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
         $data['event'] = $this->Event_model->getEventById($id_event);
+        $data['topik'] = $this->db->get('topik')->result_array();
 
         $this->load->view('User/templates/header_tryout', $data);
         $this->load->view('User/event_detail', $data);
+        $this->load->view('User/templates/footer');
+    }
+
+    public function tes_tpa($id_event)
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Tes TPA';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $data['event'] = $this->Event_model->getEventById($id_event);
+        $data['topik'] = $this->db->get_where('topik', ['id_topik' => 1])->row_array();
+        $data['topik_rule'] = $this->db->query("SELECT * from topik_rule tr left join topik t on tr.id_topik = t.id_topik")->row_array();
+
+        $harga = $this->db->select('harga')->get_where('event', ['id_event' => $id_event])->row()->harga;
+        $point = $this->db->select('point')->get_where('user', ['username' => $this->session->userdata('username')])->row()->point;
+        $username = $this->session->userdata('username');
+
+        if ($point < $harga) {
+            log_message('error', 'Point anda tidak cukup.');
+            redirect('User/tryout');
+        }
+        else{
+            $bayar = $point - $harga;
+
+            $this->db->set('point', $bayar);
+            $this->db->where('username', $username);
+            $this->db->update('user');
+
+            $this->load->view('User/templates/header_tryout', $data);
+            $this->load->view('User/tes_tpa', $data);
+            $this->load->view('User/templates/footer');
+        }
+    }
+
+    public function kerjakan_tpa($id_event)
+    {
+        $data['judul'] = 'AORTASTAN Try Out Online | Tes TPA';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $data['event'] = $this->Event_model->getEventById($id_event);
+        $data['topik'] = $this->db->get_where('topik', ['id_topik' => 1])->row_array();
+        $data['topik_rule'] = $this->db->query("SELECT * from topik_rule tr left join topik t on tr.id_topik = t.id_topik")->row_array();
+
+        $this->load->view('User/templates/header_tryout', $data);
+        $this->load->view('User/tes/kerjakan_tpa', $data);
         $this->load->view('User/templates/footer');
     }
 
---
+++ application/views/User/tes/kerjakan_tpa.php
@@ -0,0 +1,76 @@
+    <!-- END nav -->
+    <div class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url('assets/User/'); ?>images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+    	<div class="overlay"></div>
+    	<div class="container">
+    		<div class="row no-gutters slider-text align-items-end justify-content-start">
+    			<div class="col-md-12 ftco-animate text-center mb-5">
+    				<p class="breadcrumbs mb-0"><span class="mr-3">Home <i class="ion-ios-arrow-forward"></i></span><span class="mr-3">Try Out <i class="ion-ios-arrow-forward"></i></span> <span>Event</span></p>
+    				<h1 class="mb-3 bread">Tes TPA</h1>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+    <section class="ftco-section bg-light" id="soal1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 pr-lg-4">
+                    <div class="row ftco-animate">
+                        <div class="col-md-12">
+                            <div class="card">
+                              <div class="card-header">
+                                <h5>Soal No: <button type="button" class="btn btn-primary ml-2" style="width: 40px; height: 40px;">1</button></h5>
+                              </div>
+                              <div class="card-body">
+                                <form class="questionForm" id="q1" data-question="1">
+                                    <h4>Kepanjangan dari SMK adalah...</h4>
+                                    <br>
+                                    <label class="btn btn-default">
+                                        <input type="radio" name="q1" value="a"> Sekolah Maju Kawan
+                                    </label>
+                                    <br>
+                                    <label class="btn btn-default">
+                                        <input type="radio" name="q1" value="b"> Sekolah Mundur Kawan
+                                    </label>
+                                    <br>
+                                    <label class="btn btn-default">
+                                        <input type="radio" name="q1" value="c"> Sekolah Menengah Kejuruan
+                                    </label>
+                                    <br>
+                                    <label class="btn btn-default">
+                                        <input type="radio" name="q1" value="d"> Sayang Mereka Kawan
+                                    </label>
+                                    <br>
+                                    <label class="btn btn-default">
+                                        <input type="radio" name="q1" value="e"> Semua Makan Kawan
+                                    </label>
+                                </form>
+                              </div>
+                              <div class="card-footer text-muted">
+                                <a href="#" class="btn btn-info col-md-3 ml-2 mr-5" id="sb1"><i class="fas fa-chevron-left"></i> Soal Sebelumnya</a>
+                                <label class="btn btn-warning text-white col-md-3 mr-4 ml-4"><input type="checkbox"> Ragu-Ragu</label>
+                                <a href="#" class="btn btn-primary col-md-3 ml-5" id="ss1">Soal Selanjutnya <i class="fas fa-chevron-right"></i></a>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 sidebar">
+                  <div class="sidebar-box bg-white p-4 ftco-animate">
+                    <h4 class="heading-sidebar mb-3">1 dari 8 soal</h4>
+                    <form>
+                      <button type="button" class="btn btn-primary mr-4 mb-3" style="width: 40px; height: 40px;">1</button>
+                      <button type="button" class="btn btn-outline-primary mr-4 mb-3" style="width: 40px; height: 40px;">2</button>
+                      <button type="button" class="btn btn-outline-primary mr-4 mb-3" style="width: 40px; height: 40px;">3</button>
+                      <button type="button" class="btn btn-outline-primary mr-4 mb-3" style="width: 40px; height: 40px;">4</button>
+                      <button type="button" class="btn btn-outline-primary mr-4 mb-3" style="width: 40px; height: 40px;">5</button>
+                      <button type="button" class="btn btn-outline-primary mr-4 mb-3" style="width: 40px; height: 40px;">6</button>
+                      <button type="button" class="btn btn-outline-primary mr-4 mb-3" style="width: 40px; height: 40px;">7</button>
+                      <button type="button" class="btn btn-outline-primary mr-4 mb-3" style="width: 40px; height: 40px;">8</button>
+                    </form>
+
+                  </div>
+                </div>
+            </div>
+        </div>
+    </section>
